ff2023993a6f6fec28239f3087de8840d9ab4dff
Matthijs Vos <matthijsvos283@gmail.com>
2026-09-08 18:29:13 +0200

    Add interrupt test

---
+++ include/errors.h
@@ -0,0 +1,7 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#define SYSTEM_OK   (0)
+#define ERROR       (1)
+
+#endif /* ERRORS_H */
--- src/main.c
+++ src/main.c
@@ -1,25 +1,44 @@
 #include "gpio_lib.h"
 #include "usart.h"
+#include "errors.h"
+
+void buttonPD2ISR(void);
 
 int main(void) {
-    gpioPinSetDirection(PB, BIT0, OUTPUT);      // LED output
+    int32_t error = SYSTEM_OK;
 
     USART_Init();
-
-    // Configure INT0 on falling edge
-    EICRA |= (1 << ISC01);
-    EIMSK |= (1 << INT0);
-
-    sei();                   // Enable global interrupts
-
-    while (1)
-    {
-        
+    USART_Transmit('B');
+    if (gpioPinSetDirection(PB, BIT0, OUTPUT) != SYSTEM_OK) {
+        error = ERROR;
+        USART_Transmit('1');
     }
+    // else if (gpioExternalInterruptEnable(PD, BIT2, FALLING_EDGE, &buttonPD2ISR) != SYSTEM_OK) {
+    //     error = ERROR;
+    //     USART_Transmit('2');
+    // }
+    else if (gpioPinSetDirection(PB, BIT1, INPUT) != SYSTEM_OK) {
+        error = ERROR;
+        USART_Transmit('I');
+    }
+    else if (gpioPinChangeInterruptEnable(PB, BIT1, FALLING_EDGE, &buttonPD2ISR) != SYSTEM_OK) {
+        error = ERROR;
+        USART_Transmit('2');
+    }
+    else if (globalInterruptEnable() != SYSTEM_OK) {
+        error = ERROR;
+        USART_Transmit('3');
+    }
+    else {
+        while (1) {
+
+        }
+    }
+    USART_Transmit('e');
+    return error;
 }
 
-ISR(INT0_vect)
-{
-    gpioPinToggle(PB, BIT0);            // Toggle LED
+void buttonPD2ISR(void) {
+    (void)gpioPinToggle(PB, BIT0);            // Toggle LED
     USART_Transmit('x');
 }
