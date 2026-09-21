266401325d4017830cf687d8f1563a401bd28a81
Matthijs Vos <matthijsvos283@gmail.com>
2026-09-21 17:58:48 +0200

    Add segments decoded in bits

--- src/carSpeed.c
+++ src/carSpeed.c
@@ -2,6 +2,19 @@
 #include "displayBinairy.h"
 #include "errors.h"
 
+const uint8_t segments[10] = {  /* Binairy values follow this layout: 0b0GFEDCBA, with A-G representing the segments */
+    0b00111111,                 /* 0 */
+    0b00000110,                 /* 1 */
+    0b01011011,                 /* 2 */
+    0b01001111,                 /* 3 */
+    0b01100110,                 /* 4 */
+    0b01101101,                 /* 5 */
+    0b01111101,                 /* 6 */
+    0b00000111,                 /* 7 */
+    0b01111111,                 /* 8 */
+    0b01101111                  /* 9 */
+};
+
 volatile uint8_t speedSegmentBuffer[4] = {0u};
 
 int32_t carSpeedInit(void) {
